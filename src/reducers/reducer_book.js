c49a25cde5eb57c7ef5a512259f11b320e4b3703
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-03-26 15:30:38 -0700

    first commit

---
+++ src/reducers/reducer_book.js
@@ -0,0 +1,8 @@
+export default()=> {
+    return[
+        {title: 'JS: tHE GOOD PARTS' , pages: 101},
+        {title: 'Harry POtter' , pages: 99},
+        {title: 'Darl Tower' , pages: 50},
+        {title: 'Eloquent Ruby', pages: 2}
+    ];
+}
